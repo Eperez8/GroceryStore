19f9dc401a355ea7e835c7246c4e4c82cfcaa217
Eperez8 <eperez8@ecc-jaguar.csuchico.edu>
2018-10-10 17:23:38 -0700

    GroceryStore

--- main.cpp
+++ main.cpp
@@ -10,44 +10,56 @@ int main()
         string item;
         int numItems = 0;
         char input;
-        
+
 
         do{
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
+                if(list.size() != 0)
+                {
+                        cout<<"\n (D)elete last item";
+                }
+                cout<<"\n (D)elete last item";
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
                 if( input == 'a' || input == 'A')
                 {
-                       cout<<"What is the item?\n";
-                       
+                        cout<<"What is the item?\n";
+
                         cin>>item;
                         list.push_back(item);
                         numItems++;
                 }        
+                else if((input == 'd' || input == 'D') && list.size()>0)
+                {
 
-                      
-                        
-                               else if ((input == 'q' || input == 'Q') && numItems == 0)
-                            {            
-                             
-                             cout<<"No items to buy!"<<endl;
+                        item = list.back();
+                        list.pop_back();
+                        cout<<item<<" was deleted from the list.\n";
 
-                                }
 
-                        }
-                        while(!(input == 'q' || input == 'Q'));
+                }
 
-                        if(numItems != 0)
-                        {
-                                cout<<"==ITEMS TO BUY=="<<endl;
+                else if ((input == 'q' || input == 'Q') && numItems == 0)
+                {            
 
-                                for(int index = 0; index < numItems; index++)
-                               {
-                                 cout<<index+1<<" "<<list[index]<<endl;
-                                }
-  }
+                        cout<<"No items to buy!"<<endl;
+
+                }
+
+        }
+        while(!(input == 'q' || input == 'Q'));
+
+        if(numItems != 0)
+        {
+                cout<<"==ITEMS TO BUY=="<<endl;
+
+                for(int index = 0; index < numItems; index++)//List starts counting from 0
+                {
+                        cout<<index+1<<" "<<list[index]<<endl;//List starts at 1 then 2 and 3 and so on...
+                }
+        }
         return 0;
 }
